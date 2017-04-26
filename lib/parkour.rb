4909bfd89a2ed160c7012c5e9c8a823d24e0ce95
Jack Chen (chendo) <github@chen.do>
2017-04-26 15:55:13 +1000

    Handle weird cases where we get negative spaces

--- lib/parkour.rb
+++ lib/parkour.rb
@@ -5,38 +5,38 @@ module Parkour
   module_function
 
   def io
-    @io ||= begin
-      file = ENV['PARKOUR_FILE']
-      if file == 'stderr'
-        $stderr
-      elsif file
-        File.open(file, 'a').tap do |fd|
-          fd.sync = true
-        end
+    @io || $stdout
+  end
+
+  def columns
+    @columns ||= begin
+      cols = ENV.fetch('COLUMNS', `tput cols`.strip).to_i
+      if !$?.success?
+        100
       else
-        $stdout
+        cols
       end
     end
   end
 
-  def format_line(time:, line:, path:, line_no:, event: '', depth:)
+  def format_line(time:, line:, path:, line_no:, depth:, event:, return_value: nil)
     str = StringIO.new
     str << "[#{time.rjust(9)}] "
     str << "#{' ' * depth}#{line}"
+    return_string = " => #{return_value}" if return_value
+    if return_string && columns - str.string.length - return_string.length > 0
+      str << return_string 
+    end
 
-    spaces = ENV.fetch('COLUMNS', `tput cols`.strip).to_i - str.string.length
+    spaces = columns - str.string.length
     path = "#{path}:#{line_no}"
     spaces -= path.length
+    spaces = 0 if spaces < 0
     str << "#{' ' * spaces}#{path}"
     str.string
   end
 
-  CLEAR_LINE = "\e[2K"
   def begin_line(tp)
-    if @last_line
-      finish_line
-    end
-
     line = File.readlines(tp.path)[tp.lineno - 1].sub(/^\s+/, '').strip
     @last_line = {
       time: "...",
@@ -46,18 +46,22 @@ module Parkour
       line_no: tp.lineno,
       depth: @depths.shift || @depth,
     }
-    io.print Paint[format_line(**@last_line), :yellow]
     @time = Time.now.to_f
+    io.print Paint[format_line(**@last_line), :yellow]
+  rescue Errno::ENOENT
   end
 
-  def finish_line
+  CLEAR_LINE = "\e[2K"
+  def finish_line(event: nil, return_value: nil)
+    return unless @last_line
     io.print "\r#{CLEAR_LINE}"
     @last_line[:time] = "#{((Time.now.to_f - @time) * 1000).round}ms"
+    @last_line[:return_value] = return_value
     io.puts Paint[format_line(**@last_line), :green]
   end
 
   at_exit do
-    finish_line if @last_line
+    finish_line
   end
 
   def tracepoint
@@ -65,15 +69,19 @@ module Parkour
       @depths = []
       @depth = 0
       TracePoint.new(:line, :call, :return) do |tp|
-        if filters.empty? || filters.any? { |f| f.call(tp) }
+        if filters.empty? || filters.any? { |f| tp.path =~ f }
           if tp.event == :call
             @depths = [@depth, @depth + 1]
             @depth += 1
+            finish_line
             begin_line(tp)
           elsif tp.event == :return
-            @depth -= 1
+            @depth -= 1 if @depth > 0
+            return_value = tp.return_value.inspect
+            finish_line(event: :return, return_value: return_value)
             begin_line(tp)
           else
+            finish_line
             begin_line(tp)
           end
         end
@@ -82,10 +90,22 @@ module Parkour
   end
 
   def filters
-    @filters = []
+    @filters ||= []
   end
 
-  def trace(filters: [], &block)
+  def trace(filters: [], output: nil, &block)
+    file = ENV['PARKOUR_FILE']
+    @filters = filters.clone
+    @io = if file == 'stderr'
+      $stderr
+    elsif file
+      # FIXME: don't leak fds
+      File.open(file, 'a').tap do |fd|
+        fd.sync = true
+      end
+    else
+      $stdout
+    end
     tracepoint.enable
     block.call
   ensure 
--- parkour.gemspec
+++ parkour.gemspec
@@ -9,9 +9,9 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Jack Chen (chendo)"]
   spec.email         = ["github@chen.do"]
 
-  spec.summary       = %q{ Write a short summary, because Rubygems requires one.}
+  spec.summary       = %q{Line-by-line live tracing.}
   spec.description   = %q{ Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.homepage      = "https://github.com/chendo"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
