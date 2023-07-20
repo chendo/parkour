07e1876ca1933857c1cdd9058d6beaf1ac4480c6
chendo <chendo@users.noreply.github.com>
2023-07-20 23:38:49 +1000

    Add a benchmark test because turns out there's a lot of overhead in a rails app

--- spec/parkour_spec.rb
+++ spec/parkour_spec.rb
@@ -1,4 +1,6 @@
 require "spec_helper"
+require "securerandom"
+require "benchmark"
 
 describe Parkour do
   it "runs stuff" do
@@ -9,4 +11,33 @@ describe Parkour do
       3.times { sleep rand }
     end
   end
+
+  Benchmark.bm do |x|
+    x.report {
+      Parkour.trace(filters: []) do
+        it "with trace" do
+          1000.times { SecureRandom.hex(10) }
+        end
+      end
+    }
+  end
+
+  Benchmark.bm do |x|
+    x.report {
+      Parkour.trace(filters: [/NOMATCHY/]) do
+        it "with no match trace" do
+          1000.times { SecureRandom.hex(10) }
+        end
+      end
+    }
+  end
+
+  Benchmark.bm do |x|
+    x.report {
+      it "without trace" do
+        1000.times { SecureRandom.hex(10) }
+      end
+    }
+  end
+
 end
